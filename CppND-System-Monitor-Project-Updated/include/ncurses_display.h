0f821778a89ff699dc7cf5b395a3be1176c949d6
WILSON PARDI JUNIOR <wilsonjr@gol.com>
2021-11-19 13:38:39 +0900

    Add files via upload

--- CppND-System-Monitor-Project-Updated/include/ncurses_display.h
+++ CppND-System-Monitor-Project-Updated/include/ncurses_display.h
@@ -1,16 +1,19 @@
 #ifndef NCURSES_DISPLAY_H
-#define NCURSES_DISPLAY_H
 
-#include <curses.h>
+	#define NCURSES_DISPLAY_H
 
-#include "process.h"
-#include "system.h"
+	#include <curses.h>
 
-namespace NCursesDisplay {
-void Display(System& system, int n = 10);
-void DisplaySystem(System& system, WINDOW* window);
-void DisplayProcesses(std::vector<Process>& processes, WINDOW* window, int n);
-std::string ProgressBar(float percent);
-};  // namespace NCursesDisplay
+	#include "process.h"
+	#include "system.h"
+
+	namespace NCursesDisplay 
+	{
+		void Display(System& system, int n = 10);
+		void DisplaySystem(System& system, WINDOW* window);
+		void DisplayProcesses(std::vector<Process>& processes, WINDOW* window, int n);
+	
+		std::string ProgressBar(float percent);
+	};  // namespace NCursesDisplay
 
 #endif
--- CppND-System-Monitor-Project-Updated/include/processor.h
+++ CppND-System-Monitor-Project-Updated/include/processor.h
@@ -1,12 +1,46 @@
 #ifndef PROCESSOR_H
-#define PROCESSOR_H
+	#define PROCESSOR_H
+	
+	#include <string>
+	#include <vector>
+	
+	#include "linux_parser.h"
 
-class Processor {
- public:
-  float Utilization();  // TODO: See src/processor.cpp
+	class Processor 
+	{
+		public:
+			float Utilization(int number);  				// TODO: See src/processor.cpp
+			long Idle(long idle, long io_wait);
+			long NoIdle(long user, long nice, long system, long irq, long soft_irq, long steal, long guest, long guest_nice);
 
-  // TODO: Declare any necessary private members
- private:
-};
-
+		// TODO: Declare any necessary private members
+		private:
+			long user;
+			long nice;
+			long system;
+			long idle;
+			long io_wait;
+			long irq;
+			long soft_irq;
+			long steal;
+			long guest;
+			long guest_nice;
+			
+			long previous_totalNoIdleTime;
+			long previous_totalIdleTime;
+			
+			long previous_totalNoIdleTime0;
+			long previous_totalIdleTime0;
+			
+			long previous_totalNoIdleTime1;
+			long previous_totalIdleTime1;
+			
+			long previous_totalNoIdleTime2;
+			long previous_totalIdleTime2;
+			
+			long previous_totalNoIdleTime3;
+			long previous_totalIdleTime3;
+		
+			int number;
+	};
 #endif
